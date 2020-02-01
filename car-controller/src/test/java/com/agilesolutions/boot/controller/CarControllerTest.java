a5d4166d3baec71bd986820fe39b319b3bb543cd
Robert Rong <robert.rong@agile-solutions.ch>
2020-02-01 13:33:09 +0100

    f

---
+++ car-controller/src/test/java/com/agilesolutions/boot/controller/CarControllerTest.java
@@ -0,0 +1,29 @@
+package com.agilesolutions.boot.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = CarController.class)
+public class CarControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void showVresion() throws Exception {
+
+        this.mockMvc
+                .perform(get("/api/version"))
+                .andExpect(status().isOk());
+    }
+}
